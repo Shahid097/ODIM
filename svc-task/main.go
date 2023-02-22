e7d831b553bc2936d2a952bf38168fca8d7d4ab7
mohamed-shahid-afrd <mohamed-shahid.afrid@hpe.com>
2023-02-22 15:11:45 +0530

    Adding a profiling tool to task service

--- svc-task/main.go
+++ svc-task/main.go
@@ -1,20 +1,22 @@
-//(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+// (C) Copyright [2020] Hewlett Packard Enterprise Development LP
 //
-//Licensed under the Apache License, Version 2.0 (the "License"); you may
-//not use this file except in compliance with the License. You may obtain
-//a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-//WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-//License for the specific language governing permissions and limitations
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
 // under the License.
 package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/pprof"
 	"os"
 	"sync"
 	"time"
@@ -66,7 +68,11 @@ func main() {
 	for _, warning := range configWarnings {
 		log.Warn(warning)
 	}
-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/debug/profile", pprof.Profile)
+	go func() {
+		log.Fatal(http.ListenAndServe(":7777", mux))
+	}()
 	if err := dc.SetConfiguration(config.Data.MessageBusConf.MessageBusConfigFilePath); err != nil {
 		log.Fatal("error while trying to set messagebus configuration: " + err.Error())
 	}

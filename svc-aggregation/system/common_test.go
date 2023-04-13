17a4f620f1879d065eb5b93d237a12efd1a4be9b
mohamed-shahid-afrd <mohamed-shahid.afrid@hpe.com>
2023-04-13 13:12:45 +0530

    Adding more UT cases for system package

---
+++ svc-aggregation/system/common_test.go
@@ -0,0 +1,32 @@
+//(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+//
+//Licensed under the Apache License, Version 2.0 (the "License"); you may
+//not use this file except in compliance with the License. You may obtain
+//a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//License for the specific language governing permissions and limitations
+// under the License.
+
+package system
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetIPAndPortFromAddress(t *testing.T) {
+	host, port := getIPAndPortFromAddress("10.20.30.1:3000")
+	assert.Equal(t, "10.20.30.1", host, "not equal host")
+	assert.Equal(t, "3000", port, "not equal port")
+}
+
+func TestGetKeyFromManagerAddress(t *testing.T) {
+	mangrIP := getKeyFromManagerAddress("10.20.30.1:3000")
+	assert.Equal(t, "10.20.30.1:3000", mangrIP, "managerAddressIp not as expected")
+}
